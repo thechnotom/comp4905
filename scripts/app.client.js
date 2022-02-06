dd999e011852c69ff3f7e0ec12c662cf7f7dc477
Thomas Roller <thomasroller@yahoo.ca>
2022-02-06 18:56:28 -0500

    Initial test code

---
+++ scripts/app.client.js
@@ -0,0 +1,40 @@
+class App {
+
+    constructor () {
+        //
+    }
+
+    countButton () {
+        console.log("emiting 'count'");
+        this.socket.emit('count');
+    }
+
+    registerClientActions () {
+        let caller = this;
+        $('#countButton').click(function () {
+            caller.countButton();
+            console.log("count button pressed on client");
+        });
+    }
+
+    registerServerActions () {
+        this.socket.on('count', function (count) {
+            console.log("client received 'count' from server (new count: " + count.counter + " )");
+            $('#countLabel').text(count.counter);
+        });
+    }
+
+    init () {
+        //let socket = io.connect('http://localhost:3000');
+        //let socket = io.connect('0.0.0.0:3000');
+        let socket = io.connect('192.168.0.48:3000');
+        this.socket = socket;
+        this.registerClientActions();
+        this.registerServerActions();
+    }
+}
+
+$(document).ready(function () {
+    let app = new App();
+    app.init();
+});
---
+++ server.js
@@ -0,0 +1,54 @@
+import { createServer } from 'http';
+import fs, { readFile } from 'fs';
+import url, {parse} from 'url';
+import { Server } from 'socket.io';
+
+class AppServer {
+    
+    constructor () {
+        this.counter = 0;
+    }
+
+    count (socket) {
+        console.log("server received count button push");
+        this.counter += 1;
+        socket.emit('count', {"counter": this.counter});
+    }
+
+    registerSocketIO (io) {
+        let caller = this;
+
+        io.sockets.on('connection', function (socket) {
+            console.log('User connected');
+
+            socket.on('count', function () {
+                caller.count(socket);
+            });
+        });
+    }
+
+    init () {
+        let httpServer = createServer(function(req, res) {
+            let path = parse(req.url).pathname;
+            console.log(path);
+            let contentType = 'text/html';
+            if (path === '/') {
+                path = '/index.html';
+            } else if (path.indexOf('.css')) {
+                contentType = 'text/css';
+            }
+            let filePath = (new URL(import.meta.url)).pathname.replaceAll("%20", " ");
+            readFile(filePath.slice(1, filePath.lastIndexOf("/") + 1) + path, function (error, data) {
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(data, 'utf-8');
+            });
+        });
+
+        let io = new Server(httpServer);
+        this.registerSocketIO(io);
+        httpServer.listen(3000)
+    }
+}
+
+let appServer = new AppServer();
+appServer.init();
